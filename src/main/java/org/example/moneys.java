02e4acd3e7c850fb82bd467d98bf75e2453a506e
quocchung <chungquoctran96@gmail.com>
2022-12-15 16:16:25 +0700

    add new class

--- src/main/java/org/example/moneys.java
+++ src/main/java/org/example/moneys.java
@@ -1,2 +1,10 @@
-package org.example;public class moneys {
+package org.example;
+
+public class moneys {
+    public static void main(String[] args) {
+        String a = "USD";
+        String b = "VND";
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
